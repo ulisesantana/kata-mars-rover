71665353e7c929a47af438418388fad99e0ba3c7
Ulises Santana <ulisesantana@gmail.com>
2022-09-25 07:48:53 +0100

    🧪 add test to do

---
+++ src/Rover.test.ts
@@ -0,0 +1,17 @@
+describe('Rover should', () => {
+  it('start with initial starting point and the direction is facing', () => {
+
+  })
+  it.todo('receive a character array of commands')
+  it.todo('move forward')
+  it.todo('move backward')
+  it.todo('turn left')
+  it.todo('turn left')
+  describe('move across the map edges', () => {
+    it.todo('X axis on first position go backwards to last position')
+    it.todo('X axis on last position go forwards to first position')
+    it.todo('Y axis on first position go backwards to last position')
+    it.todo('Y axis on last position go forwards to first position')
+  })
+  it.todo('detects obstacles, aborts the sequence and reports the obstacle')
+})
